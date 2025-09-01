295f9dbe1f2e44c63f3354493504c0e71fa357d4
Ayush Singh <ayushsingh01042003@gmail.com>
2025-09-01 23:49:58 +0530

    Read file 8 bytes

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
 func main() {
-	
+	file, err := os.Open("data.txt")
+	if err != nil {
+		fmt.Println("Err opening file")
+		return
+	}
+
+	var output string
+	for {
+		data := make([]byte, 8)
+		_, err := file.Read(data)
+		if err != nil {
+			if err.Error() == "EOF" {
+				fmt.Printf("read: %s\n", output)
+				break
+			}
+			fmt.Println("Err reading file")
+			return
+		}
+
+		if idx := bytes.IndexRune(data, '\n'); idx != -1 {
+			output += string(data[:idx])
+			data = data[idx + 1:]
+			fmt.Printf("read: %s\n", output)
+			output = ""
+		}
+		output += string(data)
+	}
 }
