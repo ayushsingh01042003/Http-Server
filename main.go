f33916c9d2d893d51a0ab51f84113fb6584b0167
Ayush Singh <ayushsingh01042003@gmail.com>
2025-09-05 22:06:07 +0530

    Listen from network connection

--- main.go
+++ main.go
@@ -3,35 +3,60 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"os"
+	"io"
+	"net"
 )
 
 func main() {
-	file, err := os.Open("data.txt")
+	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
-		fmt.Println("Err opening file")
+		fmt.Println("Cannot start the server, ERR-", err.Error())
 		return
 	}
 
-	var output string
 	for {
-		data := make([]byte, 8)
-		_, err := file.Read(data)
+		conn, err := listener.Accept()
 		if err != nil {
-			if err.Error() == "EOF" {
-				fmt.Printf("read: %s\n", output)
-				break
-			}
-			fmt.Println("Err reading file")
-			return
+			fmt.Println("Cannot accept connection, ERR-", err)
 		}
 
-		if idx := bytes.IndexRune(data, '\n'); idx != -1 {
-			output += string(data[:idx])
-			data = data[idx + 1:]
-			fmt.Printf("read: %s\n", output)
-			output = ""
+		for line := range getLinesChannel(conn) {
+			fmt.Printf("read: %s\n", line)
 		}
-		output += string(data)
+		conn.Close()
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	strChan := make(chan string)
+
+	var output string
+	go func() {
+		defer f.Close()
+		defer close(strChan)
+		
+		for {
+			data := make([]byte, 8)
+			_, err := f.Read(data)
+
+			if err != nil {
+				if err.Error() == "EOF" {
+					strChan <- output
+					break
+				}
+				fmt.Printf("Error reading the file: %s", err.Error())
+				return
+			}
+
+			if idx := bytes.IndexRune(data, '\n'); idx != -1 {
+				output += string(data[:idx])
+				data = data[idx + 1:]
+				strChan <- output
+				output = ""
+			}
+			output += string(data)
+		}
+	}()
+
+	return strChan
 }
